23a4836f44ffb8c1a06320f3433edc63ff2a605e
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-02 12:37:57 +0600

    array and object and tuple function inmplement

---
+++ module/src/1.1.ts
@@ -0,0 +1,44 @@
+// basic data type
+
+// * inplesive  means don't mention type
+
+
+let firstName: string = 'tuhin'
+
+let number: number = 1234;
+
+let isAdmin: boolean = true;
+let x: undefined = undefined;
+
+let y: null = null;
+
+
+const names: string[] = ['tuhin', 'mahmmud']
+
+const eligibleRollList: number[] = [1, 3]
+let countTaka: number[] = [3, 5, 6]
+let myPersonalRollNumber: number[] = [925272, 851254]
+
+const rollNumber: number[] = [1, 2]
+const myRollNumber: number[] = [3, 4, 5]
+const friendsNma: string[] = ['tuhin', 'parvej']
+let myFriendsName: string[] = ['hasan', 'mahmud']
+let myOthersFriend: string[] = ['talha', 'mahmud']
+
+
+
+// tuple
+let coordinates: [number, number] = [2, 5];
+let tupleString: [number, string] = [4, 'tuhin']
+let tuple1: [number, number, string] = [5, 8, 'mahmud']
+let tuple2: [string, number, string] = ['hasan', 5, 'mahmud']
+let tuple3: [number, number, number, string, string, boolean] = [10, 30, 50, 'mahmud', 'hasan', true]
+
+tuple3[0] = 40;
+
+
+
+
+
+
+
---
+++ module/src/1.5.ts
@@ -0,0 +1,46 @@
+// object type
+
+const user: {
+    company: 'programming hero';
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'programming hero',
+    firstName: 'tuhin',
+    middleName: 'mahmud',
+    lastName: 'hasan',
+    isMarried: true
+}
+
+
+const friendsInfo: {
+    company: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'malaysia express',
+    firstName: 'parvej',
+    middleName: 'hasan',
+    lastName: 'akash',
+    isMarried: false
+}
+
+const myInformation: {
+    companyName: 'programming hero';
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    isMarried: boolean;
+
+} = {
+    companyName: 'programming hero',
+    firstName: 'mahmudul',
+    middleName: 'hasan',
+    lastName: 'tuhin',
+    isMarried: false
+
+}
