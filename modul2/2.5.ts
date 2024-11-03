f59db76cab788265baf13bcccb752ece81ed3b1f
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-04 00:12:33 +0600

    generic with object

--- modul2/2.5.ts
+++ modul2/2.5.ts
@@ -30,3 +30,24 @@ type User = { id: number; name: string }
 const res4 = createArrayWithGenericObj<User>({ id: 123, name: 'tuhin' })
 
 
+// generic with tuple 
+const createWithGenericTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2]
+}
+
+const res5 = createWithGenericTuple<string, number>('bangladesh', 123)
+const res6 = createWithGenericTuple<string, { name: string }>('bangladesh', { name: 'asia' })
+
+
+
+const addCourseStudent = <T>(student: T) => {
+    const course = 'Next Level Web Development'
+    return {
+        ...student,
+        course
+    }
+}
+
+const student1 = addCourseStudent({ name: 'tuhin', email: 'tuhin@gmali.com', id: 2894 })
+const student2 = addCourseStudent({ name: 'mahmudul', email: 'tuhin@gmali.com', id: 2894, watch: 'apple' })
+
