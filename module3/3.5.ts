e7260d4f9d5f035f36d082764b5a649e06e51b72
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-07 11:12:08 +0600

    public readonly private protected

--- module3/3.5.ts
+++ module3/3.5.ts
@@ -1,7 +1,36 @@
 {
     //
 
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
 
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+        addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+
+        getBalance() {
+            return this._balance;
+        }
+
+    }
+
+    class StudentAccount extends BankAccount {
+        test(amount: number) {
+            this._balance = this._balance + amount;
+        }
+    }
+    const richManBalance = new BankAccount(123, 'Mr. Richman', 200)
+    // richManBalance.balance = 737;
+    richManBalance.addDeposit(22)
+    const myBalance = richManBalance.getBalance()
+    console.log(myBalance);
 
 
 
